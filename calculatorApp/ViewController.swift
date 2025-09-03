3938617138c3f3bfc1681459abe2f033ed62f2d9
zzaeun <leeje57575@gmail.com>
2025-09-03 17:45:28 +0900

    Initial commit with .gitignore applied

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by 이정은 on 9/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
