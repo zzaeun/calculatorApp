2b9c526aa50a001eccab033f57bb0310c10b7785
zzaeun <leeje57575@gmail.com>
2025-09-04 19:39:53 +0900

    [Feat] Lv4 계산기 버튼 색상 구현

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -38,22 +38,22 @@ class ViewController: UIViewController {
     
     func configureButtons() {
         
-        let button1 = calculatorButton(title: "7")
-        let button2 = calculatorButton(title: "8")
-        let button3 = calculatorButton(title: "9")
-        let button4 = calculatorButton(title: "+")
-        let button5 = calculatorButton(title: "4")
-        let button6 = calculatorButton(title: "5")
-        let button7 = calculatorButton(title: "6")
-        let button8 = calculatorButton(title: "-")
-        let button9 = calculatorButton(title: "1")
-        let button10 = calculatorButton(title: "2")
-        let button11 = calculatorButton(title: "3")
-        let button12 = calculatorButton(title: "*")
-        let button13 = calculatorButton(title: "AC")
-        let button14 = calculatorButton(title: "0")
-        let button15 = calculatorButton(title: "=")
-        let button16 = calculatorButton(title: "/")
+        let button1 = calculatorButton(title: "7", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button2 = calculatorButton(title: "8", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button3 = calculatorButton(title: "9", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button4 = calculatorButton(title: "+", backgroundColor: .orange)
+        let button5 = calculatorButton(title: "4", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button6 = calculatorButton(title: "5", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button7 = calculatorButton(title: "6", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button8 = calculatorButton(title: "-", backgroundColor: .orange)
+        let button9 = calculatorButton(title: "1", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button10 = calculatorButton(title: "2", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button11 = calculatorButton(title: "3", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button12 = calculatorButton(title: "*", backgroundColor: .orange)
+        let button13 = calculatorButton(title: "AC", backgroundColor: .orange)
+        let button14 = calculatorButton(title: "0", backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button15 = calculatorButton(title: "=", backgroundColor: .orange)
+        let button16 = calculatorButton(title: "/", backgroundColor: .orange)
         
         // 가로 스택뷰 4개
         let horizontal1 = makeHorizontalStackView([button1, button2, button3, button4])
@@ -74,13 +74,14 @@ class ViewController: UIViewController {
         }
     }
     
-    func calculatorButton(title: String) -> UIButton {
+    // 버튼 UI
+    func calculatorButton(title: String, backgroundColor: UIColor) -> UIButton {
         let button = UIButton()
         
         button.setTitle(title, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 30)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        button.backgroundColor = backgroundColor
         button.layer.cornerRadius = 40
             
             
